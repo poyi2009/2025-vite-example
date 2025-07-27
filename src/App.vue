28d72ecf26768d7bf8091936985752c1f953b15c
poyi <poyi10925@gmail.com>
2025-07-27 18:54:14 +0800

    update

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<script setup>
+	import { ref } from 'vue'; //vue中用import
+	const num = ref(2);
+  function ClickMe() {
+    num.value++
+  }
+</script>
+
+<template>
+  <div>
+		{{ num }}
+    <button type="button" v-on:click="ClickMe">點我</button>
+	</div>
+</template>
+
+<style scoped>
+
+</style>
